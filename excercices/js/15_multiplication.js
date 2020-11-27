04be98928fea3e4d08a0cc8bbad2cc92acc7caf1
ornella85 <ornella.lallier@lepoles.com>
2020-11-27 14:44:39 +0100

    multiplication en js

--- excercices/js/15_multiplication.js
+++ excercices/js/15_multiplication.js
@@ -2,15 +2,17 @@
 
 for (i = 1; i <= 10; i++) {
 document.write(utilisateur + " x " + i + " = "+ utilisateur*i +"<br>");
-} */
+} 
 
+
+// MOI
 utilisateur = parseInt(prompt("Entrez un chiffre"));
 
 for (i = 1; i <= 10i++) {
 document.write(utilisateur + " x " + i + " = "+ utilisateur*i +"<br>");
 } if((i<=2) || (i >=9)){
     utilisateur = parseInt(prompt("Entrez un chiffre"));
-}
+} */
 
 
 
@@ -25,6 +27,7 @@ for (var i = 1; i <= 10; i++){
 }
 
 // exercice entre 2 et 9 boucle + condition
+
 var choix = prompt('entrez un nombre à multiplier par 10 ... entre 2 et 9');
 
 if ((choix >= 2) && (choix <= 9)){
@@ -34,7 +37,7 @@ for (var i = 1; i <= 10; i++){
 } else {
     document.write(" ce doit être un nombre compris entre 2 et 9 (inclus) ");
 }
-}
+} 
 
 
 //jordan
@@ -45,7 +48,43 @@ if ((nombre >= 2) && (nombre <= 9)) {
         var resultat = nombre * i;
         document.write(nombre + " x " + i + " = "+ resultat + "<br>");
     }
-} else {
-    alert("Entre un nombre entre 2 et 9");
+} else if( nombre >= 2) && (nombre <= 9){
     prompt("Entrez un nombre à multiplier");
+} else{
+    alert("erreur. Entre un nombre entre 2 et 9");
+} 
+
+
+
+
+// exercice entre 2 et 9 boucle WHILE 
+
+var choix = prompt('entrez un nombre à multiplier par 10 ... entre 2 et 9');
+ 
+while( choix < 2 || choix > 10){  // tandis que
+    choix = prompt("Entrez UNIQUEMENT un nombre à multiplier entre 2 et 9");
+    } if ((choix >= 2) && (choix <= 9)){ // et si
+    for (var i = 1; i <= 10; i++){  // boucle et condition
+    var multiplication = i * choix;
+    document.write(choix + "x" + i + " = " + multiplication + "<br />");
+    }
+} else {   // sinon
+    document.write(" ce doit être un nombre compris entre 2 et 9 (inclus) ");
 }
+
+
+// CORRECTION SONIA
+var choix = prompt('entrez un nombre à multiplier par 10 ... entre 2 et 9');
+
+while ((choix >1)|| (choix < 10)) {
+    if ((choix >= 2) && (choix <= 9)) {
+        for (var i = 1; i <= 10; i++) {
+            var resultat = choix * i;
+            document.write(choix + "x" + i + " = " + resultat + "<br />");
+        } 
+        break;
+    } else {
+        choix = prompt('entrez un nombre à multiplier par 10 ... entre 2 et 9');
+        }
+}
+
