ffae1e97814df7ba97a1b475cab1a29368eb3af9
ornella85 <ornella.lallier@lepoles.com>
2020-12-22 21:22:33 +0100

    ajout eval

--- webforce-3/Évaluation/evaluation_javascript_facture/js/js.js
+++ webforce-3/Évaluation/evaluation_javascript_facture/js/js.js
@@ -1,7 +1,45 @@
-function myFunction() {
-    window.print();
-}
+// HEADER - DATE
+        var jour = 0;
+        var mois = 0;
+        var annee = 2017;
 
+        for (var i = 1; i <= 31; i++) {
+            var monOption = document.createElement('option');
+            monOption.text = jour + i;
+            monOption.value = jour + i;
+     
+            document.getElementById('jour').options[i] = monOption;
+
+            var jour1 = new Date();
+            var day = jour1.getDate();
+            document.getElementById("jour").value = day;
+
+        }
+        for (var i = 1; i <= 12; i++) {
+            var monOption = document.createElement('option');
+            monOption.text = mois + i;
+            monOption.value = mois + i;
+     
+            document.getElementById('mois').options[i] = monOption;
+
+            var mois1 = new Date();
+            var month = mois1.getMonth() + 1; 
+            document.getElementById("mois").value = month;
+
+        }
+        for (var i = 1; i <= 3; i++) {
+            var monOption = document.createElement('option');
+            monOption.text = annee + i;
+            monOption.value = annee + i;
+     
+            document.getElementById('annee').options[i] = monOption;
+            
+            var annee1 = new Date();
+            var year = annee1.getFullYear();
+            document.getElementById("annee").value = year;
+        }
+        
+  
 // HEADER - Formulaire
 
 function copyContent(societe, societe2){
@@ -28,8 +66,11 @@ function copyContent(email, email2){
     receiver.value = copied.value;
 }
 
-// TABLEAU - SOUS TOTAL
 
+    
+// Bouton Imprimer
 
-
+function myFunction() {
+    window.print();
+}
 
